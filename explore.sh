be88d383acb0d76a13622f0edd9e724fa2f56499
EC2 Default User <ec2-user@ip-172-31-87-225.ec2.internal>
2024-04-08 07:14:04 +0000

    this is my third commit

--- explore.sh
+++ explore.sh
@@ -1,4 +1,5 @@
 hi i am priyadarsan ,this script is basically to  learn new things about git
 :
 i want to erite as a second commit to publish  in git
-
+:
+this is my thired commit to check for diff commamnd
